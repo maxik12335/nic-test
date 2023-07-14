6ca7ea58283910e4c0da270e24f6444bd4776f21
Maksim Kuznetsov <m.kuznecov@krediska.ru>
2023-07-14 17:37:04 +0300

    base (no vuex & router)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Form Generator App</h1>
+    <FormGenerator @submit="handleSubmit" @clear="handleClear">
+      <div class="form-data-container">
+        <div class="form-item">
+          <label for="name">Name:</label>
+          <input type="text" id="name" v-model="formData.name" />
+        </div>
+        <div class="form-item">
+          <label for="email">Email:</label>
+          <input type="email" id="email" v-model="formData.email" />
+        </div>
+        <div class="form-item">
+          <label for="message">Message:</label>
+          <textarea id="message" v-model="formData.message"></textarea>
+        </div>
+        <div class="form-item">
+          <label for="gender">Gender:</label>
+          <select id="gender" v-model="formData.gender">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+        <div class="form-item form-item-chekbox">
+          <label for="agree">Agree:</label>
+          <input type="checkbox" id="agree" v-model="formData.agree" />
+        </div>
+      </div>
+    </FormGenerator>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormGenerator from './components/FormGenerator.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormGenerator,
+  },
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        message: '',
+        gender: '',
+        agree: false,
+      },
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log('Form submitted:', this.formData);
+    },
+    handleClear() {
+      console.log('Form clear');
+      this.formData = {
+        name: '',
+        email: '',
+        message: '',
+        gender: '',
+        agree: false,
+      }
+    },
+  },
+
+  mounted() {
+    console.log("Fetch")
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+input:focus,
+textarea:focus,
+select:focus {
+  outline: none;
 }
 </style>
---
+++ src/components/FormGenerator.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="form-generator">
+    <form class="form" @submit.prevent="handleSubmit">
+      <slot></slot>
+      <button type="submit">Save</button>
+      <button type="button" @click="handleCancel">Clear</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormGenerator',
+  methods: {
+    handleSubmit() {
+      this.$emit('submit');
+    },
+    handleCancel() {
+      this.$emit('clear');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.form {
+  margin-top: 20px;
+  border: 1px solid #ccc;
+  padding: 30px;
+}
+
+.form-generator {
+  margin-bottom: 20px;
+}
+
+.form-item {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+}
+
+.form-item label {
+  margin-bottom: 5px;
+}
+
+input {
+  border: 1px solid;
+  border-radius: 3px;
+}
+
+.form-item-chekbox {
+  flex-direction: row;
+  align-items: center;
+}
+
+button {
+  margin-top: 10px;
+}
+</style>
